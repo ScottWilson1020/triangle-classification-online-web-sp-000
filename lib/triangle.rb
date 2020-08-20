d99c7948845e5f58a7a93486ec3dbe75f2cf29d8
Scott Wilson <scottwilson1020@gmail.com>
2020-08-20 18:56:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
   # write code here
+  
+  #intitialzie upon instance to take in an argument
+  
+  def initialize( :length_1, :length_2, :length_3)
+    @length_1 = :length_1 
+    @length_2 = :length_2 
+    @length_3 = :length_3
+     end 
+     
+  def kind 
 end
