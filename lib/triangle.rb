9679b03f7cb32c401abecb9a33de7504e36aff3a
Scott Wilson <scottwilson1020@gmail.com>
2020-08-20 18:58:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,11 +3,11 @@ class Triangle
   
   #intitialzie upon instance to take in an argument
   
-  def initialize( :side_1, :side_2, :length_3)
-    @length_1 = :length_1 
-    @length_2 = :length_2 
-    @length_3 = :length_3
-     end 
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
      
   def kind 
     
